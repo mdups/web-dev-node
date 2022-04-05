5e99fd77408522c9953f95c8ef1fa68490dd478b
mdups <dupee.m@northeastern.edu>
2022-04-05 11:42:37 -0400

    first commit

---
+++ my-node-project/controllers/tuits.js
@@ -0,0 +1,125 @@
+export default [
+  {
+    "_id": "123",
+    "topic": "Web Development",
+    "postedBy": {
+      "username": "ReactJS"
+    },
+    "liked": true,
+    "verified": false,
+    "handle": "ReactJS",
+    "time": "2h",
+    "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+    "tuit": " test testLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "attachments": {
+      "video": "unKvMC3Y1kI"
+    },
+    "logo-image": "../../images/reactjs.jpeg",
+    "avatarImage": "../../images/reactjs.jpeg",
+    "stats": {
+      "comments": 123,
+      "retuits": 234,
+      "likes": 345
+    },
+    "likes": 42,
+    "dislikes": 45
+
+  },
+  {
+    "_id": "567",
+    "topic": "Space",
+    "postedBy": {
+      "username": "SpaceX"
+    },
+    "liked": false,
+    "verified": false,
+    "handle": "spacex",
+    "title": "",
+    "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "attachments": {
+      "image": "../images/starship.png"
+    },
+    "time": "last month",
+    "logo-image": "../../images/nodejs.png",
+    "avatarImage": "../../images/nodejs.png",
+    "tuits": "120K",
+    "stats": {
+      "comments": 123,
+      "retuits": 234,
+      "likes": 345
+    },
+    "likes": 55,
+    "dislikes": 45
+  },
+  {
+    "_id": "234",
+    "topic": "Space",
+    "postedBy": {
+      "username": "Scott Manley"
+    },
+    "liked": true,
+    "verified": true,
+    "handle": "DJSnM",
+    "postedOn": "2020-12-10",
+    "time": "1 day",
+    "title": "JavaScript is programming language that can run on browsers as well as desktops",
+    "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "logo-image": "../../images/javascript.png",
+    "avatarImage": "../../images/javascript.png",
+    "tuits": "123K",
+    "stats": {
+      "comments": 123,
+      "retuits": 234,
+      "likes": 345
+    },
+    "likes": 2,
+    "dislikes": 45
+  },
+  {
+    "_id": "345",
+    "topic": "Web Development",
+    "postedBy": {
+      "username": "jQuery"
+    },
+    "liked": false,
+    "verified": false,
+    "handle": "jQuery",
+    "title": "jQuery",
+    "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "time": "last week",
+    "logo-image": "../../images/jquerry.png",
+    "avatarImage": "../../images/jquerry.png",
+    "tuits": "122K",
+    "stats": {
+      "comments": 123,
+      "retuits": 234,
+      "likes": 345
+    },
+    "likes": 45,
+    "dislikes": 45
+
+  },
+  {
+    "_id": "456",
+    "topic": "Web Development",
+    "postedBy": {
+      "username": "NodeJS"
+    },
+    "liked": true,
+    "verified": true,
+    "handle": "NodeJS",
+    "title": "",
+    "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "time": "last month",
+    "logo-image": "../../images/nodejs.png",
+    "avatarImage": "../../images/nodejs.png",
+    "tuits": "120K",
+    "stats": {
+      "comments": 123,
+      "retuits": 234,
+      "likes": 345
+    },
+    "likes": 400,
+    "dislikes": 45
+  }
+];
--- my-node-project/server.js
+++ my-node-project/server.js
@@ -1,5 +1,20 @@
-var express = require('express');
-var app = express();
-app.get('/hello', (req, res) =>
-  res.send('Hello World'));
-app.listen(4000);
+import express from 'express';
+import cors from 'cors';
+
+import helloController from "./controllers/hello-controller.js";
+import userController   from "./controllers/user-controller.js";
+import tuitController from "./controllers/tuits-controller.js";
+
+
+const app = express();
+app.listen(process.env.PORT || 4000);
+app.use(cors());
+app.use(express.json());
+
+
+helloController(app);
+userController(app);
+tuitController(app);
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+
