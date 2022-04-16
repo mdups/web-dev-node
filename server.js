1061749bbad0775a5219a7d0ca81213d860e4bb0
mdups <dupee.m@northeastern.edu>
2022-04-15 22:55:51 -0400

    a9

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import tuitController from "./controllers/tuits-controller.js";
 import mongoose from 'mongoose';
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://localhost:27017/webdev'
+// || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
